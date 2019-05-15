d13da7715d79c3ff5a137313d9b188849959a62b
boatmotor <boatza45@gmail.com>
2019-05-15 09:48:51 +0700

    Update testv1.php

--- testv1.php
+++ testv1.php
@@ -10,7 +10,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (($events['events'] == 'hi')) {
+if (($events[ == 'hi')) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
